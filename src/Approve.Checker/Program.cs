1fe0ea5f305d314f8e455883a7876466516d78d4
kislovdm <kislovdm@sibur.ru>
2024-02-07 08:23:53 +0300

    init

---
+++ src/Approve.Checker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+internal static class Program
+{
+    internal static Task<int> Main(string[] args)
+    {
+
+        foreach (DictionaryEntry e in System.Environment.GetEnvironmentVariables())
+        {
+            Console.WriteLine(e.Key + " = " + e.Value);
+        }
+
+        return Task.FromResult(-1);
+    }
+}
